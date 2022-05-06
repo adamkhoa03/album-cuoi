43c4eff87558dc4a12aa0ef2c8852d126c6046b8
Minh Khoa <khoanm@bssd.vn>
2022-05-06 21:35:58 +0700

    album cuoi

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html class="vi">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Minh Khoa &amp; Thi Thi</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Minh Khoa &amp; Thi Thi - Our wedding date: 2022-05-10">
+	<meta property="og:site_name" content="Minh Khoa &amp; Thi Thi">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="Đám cưới Minh Khoa &amp; Thi Thi!">
+	<meta property="og:url" content="index.html">
+	<meta property="og:description" content="Minh Khoa &amp; Thi Thi - Ngày cưới: 2022-05-10">
+	<meta property="og:image" content="ziiweb/website/626d55b593b8b218cf57a42c/thumb.jpg">
+	<meta property="og:image:url" content="ziiweb/website/626d55b593b8b218cf57a42c/thumb.jpg">
+	<meta property="og:image:secure_url" content="ziiweb/website/626d55b593b8b218cf57a42c/thumb.jpg">
+	<link rel="shortcut icon" type="image/x-icon" href="templates/template6/favicon.ico">
+	<link href="templates/template6/css/libs590f.css?v=20211023" rel="stylesheet">
+	<link href="ziiweb/css/css2.css" rel="stylesheet">
+	<link href="ziiweb/css/css3.css" rel="stylesheet">
+	<link href="ziiweb/css/css4.css" rel="stylesheet">
+	<link href='ziiweb/css/font.css' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="templates/template6/css/styleaa04.css?v=20220227">
+
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+		gtag('config', 'UA-136239187-4');
+	</script>
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+
+	<div class="loader"></div>
+	<div class="main">
+		<nav class="navbar" data-spy="affix" data-options="scrolltop:false">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-2">
+						<div id="logo"><a href="#header">K<i class="fa fa-heart"></i>T</a></div>
+					</div>
+					<div class="col-xs-10 menu-2">
+						<ul class="nav navbar-nav navbar-right">
+							<li>
+								<a class="page-scroll" href="#couple">Minh Khoa - Thi Thi</a>
+							</li>
+
+							<li>
+								<a class="page-scroll" href="#events">Sự kiện</a>
+							</li>
+
+
+							<li>
+								<a class="page-scroll" href="#gallery">Album cưới</a>
+							</li>
+
+
+
+						</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<section id="header" class="cover" data-parallax="scroll" data-image-src="ziiweb/bg.jpg">
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<div class="display-t">
+							<div class="display-tc animated">
+								<h1>LỄ THÀNH HÔN</h1>
+								<div class="banner-text">
+									<span class="banner-text_names">Minh Khoa &amp; Thi Thi</span>
+								</div>
+								<span class="banner-text_date">Ngày 10 tháng 5 năm 2022</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="couple" class="section couple">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center heading">
+						<h2>Cô dâu & Chú rể</h2>
+						<p>Và cứ thế mỗi ngày thêm một chút. Ta yêu nhau cho mãi tới muôn đời.</p>
+					</div>
+				</div>
+				<div class="row couple-wrap">
+					<div class="groom-wrap col-md-6 clearfix">
+						<div class="groom">
+							<img src="ziiweb/website/626d55b593b8b218cf57a42c/3aaee3410f4d60323cf2bafa0f69e131.jpg" alt="groom" class="img-responsive">
+						</div>
+						<div class="desc-groom">
+							<h3>Minh Khoa</h3>
+							<p>Cuộc sống thật ý nghĩa khi chúng ta bên nhau</p>
+							<div class="social">
+
+							</div>
+						</div>
+					</div>
+					<p class="heart text-center"><i class="fa fa-heart"></i></p>
+					<div class="bride-wrap col-md-6">
+						<div class="bride">
+							<img src="ziiweb/website/626d55b593b8b218cf57a42c/a51bccd3d3565fa9208a347a2c381eee.jpg" alt="groom" class="img-responsive">
+						</div>
+						<div class="desc-bride">
+							<h3>Thi Thi</h3>
+							<p>Ngày hạnh phúc nhất trên đời là ngày được chung đôi cùng anh</p>
+							<div class="social">
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="countdown bg section text-center" style="background-image:url(ziiweb/website/626d55b593b8b218cf57a42c/templates/6174ee7a7a1c7661fb0a671c/bf4d8f99d9888da91cbbae2750ca5335.jpg);">
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row">
+					<div class="heading col-sm-12">
+						<h2>Thời gian đếm ngược còn lại<br>
+						</h2>
+						<p></p>
+					</div>
+					<div class="col-sm-12">
+						<div id="timer" class="timer text-center">
+							<div class="timer-item_digits" id="clock" data-date="2022-05-10 11:00:00" data-text-day="Ngày" data-text-hour="Giờ" data-text-minute="Phút" data-text-second="Giây"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="events" class="events-accordion section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center heading">
+						<h2>Sự Kiện Cưới</h2>
+						<p>Tình yêu không làm cho thế giới quay tròn. Tình yêu là những gì làm cho chuyến đi đáng giá.
+						</p>
+					</div>
+				</div>
+				<div class="row event-list">
+					<div class="event-item col-lg-3 col-md-6 col-xs-12">
+						<div class="event-item-box">
+							<img src="ziiweb/default/website/7744fc9739685fe61c53cd8fe2cf7e52.png" alt="LỄ CƯỚI NHÀ NỮ">
+							<div class="event-item_details">
+								<h3 style="margin-top: 10px;">LỄ CƯỚI NHÀ GÁI</h3>
+								<h4><strong>16:30 09/05/2022</strong></h4>
+								<h4>Liên Tài Năng - Tùng Lộc - Can Lộc - Hà Tĩnh</h4>
+								<a href="https://www.google.com/maps/place/Th%C3%B4n+Li%C3%AAn+T%C3%A0i+N%C4%83ng/@18.4610942,105.8165001,17z/data=!3m1!4b1!4m5!3m4!1s0x31384bf262125011:0x4e2f6b0e61daaf33!8m2!3d18.4610891!4d105.8186888?hl=vi" target="_blank" class="btn btn-danger btn-sm">Xem bản đồ</a>
+							</div>
+						</div>
+					</div>
+					<div class="event-item col-lg-3 col-md-6 col-xs-12">
+						<div class="event-item-box">
+							<img src="ziiweb/default/website/eee1cb36b560f0d80f513c4e9be666db.png" alt="LỄ CƯỚI NHÀ NAM">
+							<div class="event-item_details">
+								<h3 style="margin-top: 10px;">LỄ CƯỚI NHÀ TRAI</h3>
+								<h4><strong>11:00 10/05/2022</strong></h4>
+								<h4>Tân An - Gia Lập - Gia Viễn - Ninh Bình</h4>
+								<a href="https://www.google.com/maps/place/Tân+An,+Gia+Lập,+Gia+Viễn,+Ninh+Bình,+Việt+Nam/@20.3401227,105.8951783,18z/data=!3m1!4b1!4m5!3m4!1s0x313678bcfedfa9b3:0x8b089c2a98971ad!8m2!3d20.3402994!4d105.8961919?hl=vi-VN" target="_blank" class="btn btn-danger btn-sm">Xem bản đồ</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+		<section id="gallery" class="section gallery">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center heading">
+						<h2>Album Hình Cưới</h2>
+						<p>Được ai đó yêu sâu sắc sẽ mang lại cho bạn sức mạnh, trong khi yêu ai đó sâu sắc sẽ cho bạn
+							dũng khí.</p>
+						<div class="text-center">
+							<a href="photo-album.html" target="_blank" class="btn btn-danger btn-sm">Xem Album</a>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-12">
+
+						<div class="grid grid--margin">
+							<div class="grid-sizer"></div>
+							<div class="grid-item grid-item--width moments friends">
+								<a href="ziiweb/website/626d55b593b8b218cf57a42c/galleries/626d5a4e6bbf073f210260b3/large.jpg" data-lightbox="roadtrip">
+									<img src="ziiweb/website/626d55b593b8b218cf57a42c/galleries/626d5a4e6bbf073f210260b3/small.jpg" alt="gallery">
+								</a>
+							</div>
+							<div class="grid-item grid-item--width moments friends">
+								<a href="ziiweb/website/626d55b593b8b218cf57a42c/galleries/626d599e4eec3c3b25138213/large.jpg" data-lightbox="roadtrip">
+									<img src="ziiweb/website/626d55b593b8b218cf57a42c/galleries/626d599e4eec3c3b25138213/small.jpg" alt="gallery">
+								</a>
+							</div>
+							<div class="grid-item grid-item--width moments friends">
+								<a href="ziiweb/website/626d55b593b8b218cf57a42c/galleries/626d5933cad7ed07ab19f9eb/large.jpg" data-lightbox="roadtrip">
+									<img src="ziiweb/website/626d55b593b8b218cf57a42c/galleries/626d5933cad7ed07ab19f9eb/small.jpg" alt="gallery">
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+	<script src="templates/template6/js/jquery-plugin-collectionb7f5.js?v=20220109"></script>
+	<script src="templates/template6/js/script2a44.js?v=202202262"></script>
+</body>
+
+</html>
